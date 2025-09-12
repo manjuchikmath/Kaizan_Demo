ba1683b4f55b5cfbd177753b5e7861e49a962f21
manjuchikmath <manju.c8389@gmail.com>
2025-09-12 14:50:55 +0530

    1first commit

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 # S3-only remote backend (NO DynamoDB locking).
 # Suitable for single-user or simple CI where runs won't overlap.
+
 terraform {
   backend "s3" {
-    bucket  = "tf-state-<YOUR-ACCOUNT-ID>-ap-south-1"  # e.g. tf-state-123456789012-ap-south-1
+    bucket  = "tf-state-043976678532-ap-south-1" # 👈 your bucket name
     key     = "landing-zone/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
